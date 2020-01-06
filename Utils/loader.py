a55ead10955190d3492fbe01cfa0ce33076d6d28
surajgattani <suraj@Surajs-MacBook-Pro.local>
2020-01-06 10:55:29 -0500

    Added learning rate decay

--- Utils/loader.py
+++ Utils/loader.py
@@ -7,34 +7,34 @@ import pandas as pd
 
 AA_LS = 'ACDEFGHIKLMNPQRSTVWY'
 
-def synthetic_data(num_samples=1000, seq_len=10, aa_list=AA_LS):
 
+def synthetic_data(num_samples=1000, seq_len=10, aa_list=AA_LS):
     aa_size = len(aa_list)
-    data = np.zeros((num_samples,seq_len),dtype=int)
-    out = np.zeros((num_samples),dtype=int)
+    data = np.zeros((num_samples, seq_len), dtype=int)
+    out = np.zeros((num_samples), dtype=int)
     for i in range(num_samples):
         for j in range(seq_len):
-            data[i,j]=np.random.randint(aa_size)
+            data[i, j] = np.random.randint(aa_size)
     for i in range(num_samples):
-        if i<round(num_samples / 2):
+        if i < round(num_samples / 2):
             out[i] = 1
 
     return data, out
 
+
 def encode_data(data, aa_list=AA_LS):
-    
     codes = np.eye(len(aa_list))
     x = codes[data]
     return x
 
-def train_test_loader(x, y, test_size=0.3, batch_size=16):
 
+def train_test_loader(x, y, test_size=0.3, batch_size=16):
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test_size, shuffle=True)
 
     x_tensor = torch.from_numpy(X_train).float()
     y_tensor = torch.from_numpy(y_train).float()
-    train_dataset = torch.utils.data.TensorDataset(x_tensor, y_tensor)
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size)
+    train_dataset = data.TensorDataset(x_tensor, y_tensor)
+    train_loader = data.DataLoader(dataset=train_dataset, batch_size=batch_size)
 
     x_tensor = torch.from_numpy(X_test).float()
     y_tensor = torch.from_numpy(y_test).float()
--- Utils/model.py
+++ Utils/model.py
@@ -2,163 +2,189 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import loader
-import os
+import os, json
 import numpy as np
-from sklearn.metrics import confusion_matrix, matthews_corrcoef, roc_auc_score, accuracy_score
-import json
+from sklearn.metrics import confusion_matrix, matthews_corrcoef, accuracy_score
+
 
 class Model(nn.Module):
 
-  def __init__(self, param_dict, *args, **kwargs):
-    super(Model, self).__init__()
+    def __init__(self, para_dict, *args, **kwargs):
+        super(Model, self).__init__()
 
-    self.param_dict = param_dict
-    self.model_name = self.param_dict['model_name']
-    self.workpath = os.path.join(os.getcwd(),'work')
-    self.seq_len = self.param_dict['seq_len']
-    self.modelnamepath = os.path.join(self.workpath,self.model_name)
-    self.modelpath = os.path.join(self.modelnamepath,'model')
-    self.epoch = self.param_dict['epoch']
-    self.batch_size = self.param_dict['batch_size']
-    self.learning_rate = self.param_dict['learning_rate']
-    self.optim_name = self.param_dict['optim_name']
+        self.para_dict = para_dict
+        self.workpath = os.path.join(os.getcwd(), 'work')
 
-    if not os.path.exists(self.workpath):
-      os.mkdir(self.workpath)
-      if not os.path.exists(self.modelnamepath):
-        os.mkdir(self.modelnamepath)
+        if 'model_name' not in para_dict:
+            self.model_name = 'Model'
+        else:
+            self.model_name = self.para_dict['model_name']
+        if 'seq_len' not in para_dict:
+            self.seq_len = 10
+        else:
+            self.seq_len = self.para_dict['seq_len']
+        if 'epoch' not in para_dict:
+            self.epoch = 50
+        else:
+            self.epoch = self.para_dict['epoch']
+        if 'batch_size' not in para_dict:
+            self.batch_size = 20
+        else:
+            self.batch_size = self.para_dict['batch_size']
+        if 'step_size' not in para_dict:
+            self.step_size = 5
+        else:
+            self.step_size = self.para_dict['step_size']
+        if 'learning_rate' not in para_dict:
+            self.learning_rate = 0.01
+        else:
+            self.learning_rate = self.para_dict['learning_rate']
+        if 'optim_name' not in para_dict:
+            self.optim_name = 'Adam'
+        else:
+            self.optim_name = self.para_dict['optim_name']
 
-  def net_init(self):
+        self.modelnamepath = os.path.join(self.workpath, self.model_name)
+        self.modelpath = os.path.join(self.modelnamepath, 'model')
 
-    self.fc = nn.Linear(20*self.seq_len, 2)
+        if not os.path.exists(self.workpath):
+            os.mkdir(self.workpath)
+        if not os.path.exists(self.modelnamepath):
+            os.mkdir(self.modelnamepath)
 
-  def forward(self, input1):
+    def net_init(self):
 
-    input1 = input1.view(input1.shape[0], 20*self.seq_len)
-    input1 = self.fc(input1)
-    input1 = torch.sigmoid(input1)
+        self.fc = nn.Linear(20 * self.seq_len, 2)
 
-    return input1
+    def forward(self, x):
 
-  def objective(self):
-    return nn.CrossEntropyLoss()
+        x = x.view(x.shape[0], 20 * self.seq_len)
+        x = self.fc(x)
+        x = torch.sigmoid(x)
 
-  def optimizers(self):
+        return x
 
-    if self.optim_name == 'Adam':
-      return optim.Adam(self.parameters(), lr=self.learning_rate)
+    def objective(self):
+        return nn.CrossEntropyLoss()
 
-    elif self.optim_name == 'RMSprop':
-      return optim.RMSprop(self.parameters(), lr=self.learning_rate)
+    def optimizers(self):
 
-    elif self.optim_name == 'SGD':
-      return optim.SGD(self.parameters(), lr=self.learning_rate)   
+        if self.optim_name == 'Adam':
+            return optim.Adam(self.parameters(), lr=self.learning_rate)
 
-  def fit(self, train_loader):
+        elif self.optim_name == 'RMSprop':
+            return optim.RMSprop(self.parameters(), lr=self.learning_rate)
 
-    saved_epoch = 0
-    self.net_init()
-    if self.load_model() is not None:
-      saved_epoch = self.load_model()
-    
-    self.train()
-    optimizer = self.optimizers()
-    total_loss=i=0
-    for e in range(saved_epoch, self.epoch):
-      for features, labels in train_loader:
-          outputs_train = []
-          logps = self.forward(features)
-          loss = self.objective()
-          loss = loss(logps,labels.type(torch.long))
-          total_loss+=loss
-          i+=1
-          outputs_train.append(logps.detach().numpy())
-          optimizer.zero_grad()
-          loss.backward()
-          optimizer.step()
-      
-      self.save_model('Epoch_'+str(e+1),self.state_dict())
-      print('Epoch: %d: Loss=%.3f'%(e+1,total_loss/i))
+        elif self.optim_name == 'SGD':
+            return optim.SGD(self.parameters(), lr=self.learning_rate)
 
-  def predict(self, test_loader):
+    def fit(self, train_loader):
 
-    self.eval()
-    test_loss = 0
-    outputs_test = []
-    labels_test = []
-    with torch.no_grad():
-        for data_1 in test_loader:
-            inputs, labels = data_1
-            outputs = self.forward(inputs)
-            batch_loss = self.objective()
-            batch_loss = batch_loss(outputs, labels.type(torch.long))
-            test_loss += batch_loss.item()
-            outputs_test.append(outputs.detach().numpy())
-            labels_test.append(labels)
+        self.net_init()
+        saved_epoch = self.load_model()
 
-        return outputs_test, labels_test
+        self.train()
+        optimizer = self.optimizers()
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=self.step_size, gamma=0.5 ** (self.epoch / self.step_size))
+        total_loss = i = 0
+        for e in range(saved_epoch, self.epoch):
+            for features, labels in train_loader:
+                outputs_train = []
+                logps = self.forward(features)
+                loss = self.objective()
+                loss = loss(logps, labels.type(torch.long))
+                total_loss += loss
+                i += 1
+                outputs_train.append(logps.detach().numpy())
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
 
-  def evaluate(self, outputs, labels):
+            self.save_model('Epoch_' + str(e + 1), self.state_dict())
+            print('Epoch: %d: Loss=%.3f' % (e + 1, total_loss / i))
 
-    y_pred = []
-    for a in outputs:
-      y_pred.append(a[0][1]/(a[0][1]+a[0][0]))
-    y_true = np.array(labels)
-    y_pred = np.array(y_pred).round()
-    mat = confusion_matrix(y_true, y_pred)
-    acc = accuracy_score(y_true, y_pred)
-    mcc = matthews_corrcoef(y_true, y_pred)
+    def predict(self, test_loader):
 
-    print('Test: ')
-    print(mat)
-    print('Accuracy = %.3f ,MCC = %.3f' %(acc,mcc))
+        self.eval()
+        test_loss = 0
+        outputs_test = []
+        labels_test = []
+        with torch.no_grad():
+            for data_1 in test_loader:
+                inputs, labels = data_1
+                outputs = self.forward(inputs)
+                batch_loss = self.objective()
+                batch_loss = batch_loss(outputs, labels.type(torch.long))
+                test_loss += batch_loss.item()
+                outputs_test.append(outputs.detach().numpy())
+                labels_test.append(labels)
 
-    return mat, acc, mcc
+            return outputs_test, labels_test
 
-  def save_model(self, filename, model):
+    def evaluate(self, outputs, labels):
 
-    if not os.path.exists(self.modelpath):
-      os.mkdir(self.modelpath)
-      torch.save(model,os.path.join(self.modelpath,filename))
-    else:
-      torch.save(model,os.path.join(self.modelpath,filename))
+        y_pred = []
+        for a in outputs:
+            y_pred.append(a[0][1] / (a[0][1] + a[0][0]))
+        y_true = np.array(labels)
+        y_pred = np.array(y_pred).round()
+        mat = confusion_matrix(y_true, y_pred)
+        acc = accuracy_score(y_true, y_pred)
+        mcc = matthews_corrcoef(y_true, y_pred)
 
-  def load_model(self):
+        print('Test: ')
+        print(mat)
+        print('Accuracy = %.3f ,MCC = %.3f' % (acc, mcc))
 
-    for e in range(self.epoch,0,-1):
-      if os.path.isfile(os.path.join(self.modelpath,'Epoch_'+str(e))):
-        self.load_state_dict(torch.load(os.path.join(self.modelpath,'Epoch_'+str(e))))
-        return e
-    return None
+        return mat, acc, mcc
 
-  def save_param(self, path, param_dict):
-    with open(os.path.join(path, 'train_parameters.json'), 'w') as f:
-      json.dump(param_dict, f, indent=2)
+    def save_model(self, filename, model):
 
-  def load_param(self, path):
-    if os.path.exists(os.path.join(path,'train_parameters.json')):
-      return json.load(open(os.path.join(path,'train_parameters.json'), 'r'))
-    return None
+        if not os.path.exists(self.modelpath):
+            os.mkdir(self.modelpath)
+            torch.save(model, os.path.join(self.modelpath, filename))
+        else:
+            torch.save(model, os.path.join(self.modelpath, filename))
+
+    def load_model(self):
+
+        for e in range(self.epoch, 0, -1):
+            if os.path.isfile(os.path.join(self.modelpath, 'Epoch_' + str(e))):
+                print(os.path.join(self.modelpath, 'Epoch_' + str(e)))
+                self.load_state_dict(torch.load(os.path.join(self.modelpath, 'Epoch_' + str(e))))
+                return e
+        return 0
+
+    def save_param(self, path, para_dict):
+        with open(os.path.join(path, 'train_parameters.json'), 'w') as f:
+            json.dump(para_dict, f, indent=2)
+
+    def load_param(self, path):
+        if os.path.exists(os.path.join(path, 'train_parameters.json')):
+            return json.load(open(os.path.join(path, 'train_parameters.json'), 'r'))
+        return None
+
 
 if __name__ == '__main__':
 
-  param_dict = {'num_samples':1000,
-                'seq_len':20,
-                'batch_size':20,
-                'model_name':'Model',
-                'optim_name':'Adam',
-                'epoch':50,
-                'learning_rate':0.01}
+    para_dict = {'num_samples': 1000,
+                 'seq_len': 20,
+                 'batch_size': 20,
+                 'model_name': 'Model',
+                 'optim_name': 'Adam',
+                 'step_size': 10,
+                 'epoch': 25,
+                 'learning_rate': 0.01}
 
-  data, out = loader.synthetic_data(num_samples=param_dict['num_samples'], seq_len=param_dict['seq_len'])
-  data = loader.encode_data(data)
-  train_loader, test_loader = loader.train_test_loader(data, out, test_size=0.3, batch_size=param_dict['batch_size'])
-  model = Model(param_dict)
+    data, out = loader.synthetic_data(num_samples=para_dict['num_samples'], seq_len=para_dict['seq_len'])
+    data = loader.encode_data(data)
+    train_loader, test_loader = loader.train_test_loader(data, out, test_size=0.3, batch_size=20)
+    model = Model(para_dict)
 
-  if model.load_param(model.modelnamepath) is None:
-    model.save_param(model.modelnamepath, model.param_dict)
+    if model.load_param(model.modelnamepath) is None:
+        model.save_param(model.modelnamepath, model.para_dict)
+        model.fit(train_loader)
 
-  model.fit(train_loader)
-
-  out_test, labels_test = model.predict(test_loader)
-  mat, acc, mcc = model.evaluate(out_test, labels_test)
+    out_test, labels_test = model.predict(test_loader)
+    mat, acc, mcc = model.evaluate(out_test, labels_test)
