d3745d3bf0ba9ac7e053d216bd61cc1c807d136c
surajgattani <suraj@Surajs-MacBook-Pro.local>
2020-01-26 15:27:22 -0500

    Update OAS_loader and adding the predict function for Bi_LSTM

--- Benchmarks/OAS_dataset/data/OAS_data_loader.py
+++ Benchmarks/OAS_dataset/data/OAS_data_loader.py
@@ -1,20 +1,22 @@
-from torch.utils.data import IterableDataset, DataLoader
+from torch.utils.data import IterableDataset, DataLoader, Dataset
 import pandas as pd
+import numpy as np
 import os
-from itertools import cycle, islice
+from itertools import cycle, islice, chain
 
 valid_fields = ['Age', 'BSource', 'BType', 'Chain', 'Disease', 'Isotype', \
                 'Link', 'Longitudinal', 'Species', 'Subject', 'Vaccine']
 input_type_dict = {'FR1': 'FW1-IMGT', 'FR2': 'FW2-IMGT', 'FR3': 'FW3-IMGT', \
                    'FR4': 'FW4-IMGT', 'CDR1': 'CDR1-IMGT', 'CDR2': 'CDR2-IMGT', \
                    'CDR3': 'CDR3-IMGT'}
-full_seq_order = ['FW1-IMGT', 'CDR1-IMGT', 'FW2-IMGT', 'CDR2-IMGT',\
+full_seq_order = ['FW1-IMGT', 'CDR1-IMGT', 'FW2-IMGT', 'CDR2-IMGT', \
                   'FW3-IMGT', 'CDR3-IMGT', 'FW4-IMGT']
 AA_LS = 'ACDEFGHIKLMNPQRSTVWY'
 AA_GP = 'ACDEFGHIKLMNPQRSTVWY-'
 
+
 class OAS_Dataset(IterableDataset):
-    def __init__(self, list_IDs, labels, input_type, gapped = True, seq_dir = './seq_db/'):
+    def __init__(self, list_IDs, labels, input_type, gapped=True, seq_dir='./seq_db/'):
         '''
         list_IDs: file name (prefix) for the loader
         labels: a dictionary, specifying the output label for each file
@@ -29,7 +31,13 @@ class OAS_Dataset(IterableDataset):
         self.gapped = gapped
         self.seq_dir = seq_dir
 
-
+    def encode_index(self, data, aa_list='ACDEFGHIKLMNPQRSTVWY-'):
+        aa_list = list(aa_list)
+        X = np.zeros((len(data), len(data[0])), dtype=int)
+        for i, seq in enumerate(data):
+            for j, s in enumerate(seq):
+                X[i, j] = aa_list.index(s)
+        return X
 
     def parse_file(self):
         # load data file
@@ -37,39 +45,52 @@ class OAS_Dataset(IterableDataset):
         for m in range(len(input_fnames)):
             input_fname = input_fnames[m]
             ID = self.list_IDs[m]
-            input_df = pd.read_csv(input_fname, sep = '\t')
+            input_df = pd.read_csv(input_fname, sep='\t')
             input_df = input_df.fillna('')
             # transformation (keep the gaps or filter out the gaps; extract/assemble sequences)
             if self.input_type in input_type_dict:
                 X = input_df[input_type_dict[self.input_type]].values
             elif self.input_type == 'CDR3_full':
                 X = [input_df['CDR3-IMGT'].iloc[nn][:7] + input_df['CDR3-IMGT-111-112'].iloc[nn] + \
-                         input_df['CDR3-IMGT'].iloc[nn][7:] for nn in range(len(input_df))]
+                     input_df['CDR3-IMGT'].iloc[nn][7:] for nn in range(len(input_df))]
             elif self.input_type == 'full_length':
                 X = [''.join([input_df[item].iloc[kk] for item in full_seq_order]) for kk in range(len(input_df))]
                 X = [X[nn][:112] + input_df['CDR3-IMGT-111-112'].iloc[nn] + \
-                         X[nn][112:] for nn in range(len(input_df))]
+                     X[nn][112:] for nn in range(len(input_df))]
             else:
                 print('invalid seq type!')
                 return
 
             if not self.gapped:
-                X = [item.replace('-','') for item in X]
+                X = [item.replace('-', '') for item in X]
 
+            X = self.encode_index(X)
             y = [self.labels[ID] for _ in range(len(input_df))]
+            # print(len(y))
 
-            yield zip(X, y)
+            # print(len(X))
+            yield X
 
     def get_stream(self):
         return chain.from_iterable(islice(self.parse_file(), len(self.list_IDs)))
+    #     m = islice(self.parse_file(), 5)
+    #     return m
 
     def __iter__(self):
         return self.get_stream()
 
-#-------------------------------
-def OAS_data_loader(index_file, output_field, input_type, \
-                    gapped =True, seq_dir = './seq_db/', batch_size=16):
-    index_df = pd.read_csv(index_file, sep = '\t')
+
+# -------------------------------
+def collate_fn(batch):
+    return batch, [x for seq in batch for x in seq]
+
+def OAS_data_loader(index_file, output_field, input_type, gapped=True, seq_dir='./Models_generation/Benchmarks/OAS_dataset/data/seq_db/'):
+    index_df = pd.read_csv(index_file, sep='\t')
+    index_df = index_df[index_df.valid_entry_num>=50000].take([0,1,2])
+        # set_index("valid_entry_num").drop(0, axis=0).drop(1, axis=0) #.take([0,1,2,15,16,17,18])
+    train_df = index_df[index_df.Species=='human']
+
+    print(train_df)
 
     # Datasets
     if output_field in valid_fields:
@@ -78,18 +99,10 @@ def OAS_data_loader(index_file, output_field, input_type, \
         print('invalid output type!')
         return
 
-    partition = {'train': [], 'test': []}  # IDs, to be done!
+    partition = {'train': train_df['file_name'].values, 'test': index_df['file_name'].values}  # IDs, to be done!
 
     # generators
     training_set = OAS_Dataset(partition['train'], labels, input_type, gapped, seq_dir)
-    training_generator = DataLoader(training_set, batch_size = batch_size)
-
     testing_set = OAS_Dataset(partition['test'], labels, input_type, gapped, seq_dir)
-    testing_generator = DataLoader(testing_set, batch_size = batch_size)
 
-    return training_generator, testing_generator
-    
-
-
-
-
+    return training_set, testing_set
--- Models/Wollacott2019/Bi_LSTM.py
+++ Models/Wollacott2019/Bi_LSTM.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 from ...Utils.model import Model
 from ...Utils import loader
+from ...Benchmarks.OAS_dataset.data import OAS_data_loader
 import numpy as np
 from sklearn.metrics import confusion_matrix, matthews_corrcoef, accuracy_score
 
@@ -19,7 +20,7 @@ aa2id_i = { True: dict(zip(vocab_i[True],  list(range(len(vocab_i[True]))))),
 id2aa_i = { True: dict(zip(list(range(len(vocab_i[True]))),  vocab_i[True])),
            False: dict(zip(list(range(len(vocab_i[False]))), vocab_i[False]))}
 
-    
+
 def collate_fn(batch):
     return batch, [x for seq in batch for x in seq]
 
@@ -55,22 +56,20 @@ class LSTM_Bi(Model):
         self.fc3 = nn.Linear(self.para_dict['hidden_dim'], self.para_dict['out_dim'])
         self.fixed_len = self.para_dict['fixed_len']
         self.forward = self.forward_flen if self.fixed_len else self.forward_vlen
-        
+
     def forward_flen(self, Xs):
         # batch_size = len(Xs)
-        batch_size = self.para_dict['batch_size']
+        batch_size = len(Xs)
         # print(_aa2id)
         _aa2id =  aa2id_i[para_dict['gapped']]
 
-        # print(Xs)
         # pad <EOS> & <SOS>
         Xs_f = [[_aa2id['<SOS>']] + list(seq)[:-1] for seq in Xs]
         Xs_b = [[_aa2id['<EOS>']] + list(seq)[::-1][:-1] for seq in Xs]
-        
+
         # get sequence lengths
         X_len = len(Xs_f[0])
 
-        # print(Xs_f)
         # list to *.tensor
         Xs_f = torch.tensor(Xs_f)
         Xs_b = torch.tensor(Xs_b)
@@ -89,10 +88,10 @@ class LSTM_Bi(Model):
         # lstm
         lstm_out_f, _ = self.lstm_f(Xs_f, ini_hc_state_f)
         lstm_out_b, _ = self.lstm_b(Xs_b, ini_hc_state_b)
-        
+
         # flatten forward-lstm output
         lstm_out_f = lstm_out_f.reshape(-1, self.para_dict['hidden_dim'])
-        
+
         # flatten backward-lstm output
         idx_b = torch.tensor(list(range(X_len))[::-1])
         lstm_out_b = torch.index_select(lstm_out_b, 1, idx_b)
@@ -105,7 +104,7 @@ class LSTM_Bi(Model):
         out = F.relu(self.fc2(out))
         out = self.fc3(out)
         # print(out.shape)
-        
+
         # compute scores
         scores = F.log_softmax(out, dim=1)
 
@@ -116,29 +115,29 @@ class LSTM_Bi(Model):
         _aa2id = aa2id_i[para_dict['gapped']]
 
         # pad <EOS> & <SOS>
-        Xs_f = [[_aa2id['<SOS>']] + seq[:-1] for seq in Xs]
-        Xs_b = [[_aa2id['<EOS>']] + seq[::-1][:-1] for seq in Xs]
-        
+        Xs_f = [[_aa2id['<SOS>']] + list(seq)[:-1] for seq in Xs]
+        Xs_b = [[_aa2id['<EOS>']] + list(seq)[::-1][:-1] for seq in Xs]
+
         # get sequence lengths
         Xs_len = [len(seq) for seq in Xs_f]
         lmax = max(Xs_len)
-        
+
         # list to *.tensor
         Xs_f = [torch.tensor(seq) for seq in Xs_f]
         Xs_b = [torch.tensor(seq) for seq in Xs_b]
-        
+
         # padding
         Xs_f = pad_sequence(Xs_f, batch_first=True)
         Xs_b = pad_sequence(Xs_b, batch_first=True)
-        
+
         # embedding
-        Xs_f = self.word_embeddings(Xs_f)
-        Xs_b = self.word_embeddings(Xs_b)
-        
+        Xs_f = self.word_embeddings(Xs_f.type(dtype=torch.LongTensor))
+        Xs_b = self.word_embeddings(Xs_b.type(dtype=torch.LongTensor))
+
         # packing the padded sequences
         Xs_f = pack_padded_sequence(Xs_f, Xs_len, batch_first=True, enforce_sorted=False)
         Xs_b = pack_padded_sequence(Xs_b, Xs_len, batch_first=True, enforce_sorted=False)
-        
+
         # feed the lstm by the packed input
         ini_hc_state_f = (torch.zeros(1, batch_size, self.para_dict['hidden_dim']),
                           torch.zeros(1, batch_size, self.para_dict['hidden_dim']))
@@ -147,81 +146,85 @@ class LSTM_Bi(Model):
 
         lstm_out_f, _ = self.lstm_f(Xs_f, ini_hc_state_f)
         lstm_out_b, _ = self.lstm_b(Xs_b, ini_hc_state_b)
-        
+
         # unpack outputs
         lstm_out_f, lstm_out_len = pad_packed_sequence(lstm_out_f, batch_first=True)
         lstm_out_b, _            = pad_packed_sequence(lstm_out_b, batch_first=True)
-        
+
         lstm_out_valid_f = lstm_out_f.reshape(-1, self.para_dict['hidden_dim'])
-        lstm_out_valid_b = lstm_out_b.reshape(-1, self.para_dict['hidden_dim'])    
-        
+        lstm_out_valid_b = lstm_out_b.reshape(-1, self.para_dict['hidden_dim'])
+
         idx_f = []
         [idx_f.extend([i*lmax+j for j in range(l)]) for i, l in enumerate(Xs_len)]
         idx_f = torch.tensor(idx_f)
-        
+
         idx_b = []
         [idx_b.extend([i*lmax+j for j in range(l)][::-1]) for i, l in enumerate(Xs_len)]
         idx_b = torch.tensor(idx_b)
-        
+
         lstm_out_valid_f = torch.index_select(lstm_out_valid_f, 0, idx_f)
         lstm_out_valid_b = torch.index_select(lstm_out_valid_b, 0, idx_b)
-        
-        lstm_out_valid = lstm_out_valid_f + lstm_out_valid_b       
-        
+
+        lstm_out_valid = lstm_out_valid_f + lstm_out_valid_b
+
         # lstm hidden state to output space
         out = F.relu(self.fc1(lstm_out_valid))
         out = F.relu(self.fc2(out))
         out = self.fc3(out)
-        
+
         # compute scores
         scores = F.log_softmax(out, dim=1)
         print(scores)
-        
+
         return scores
-    
+
     def objective(self):
         return nn.NLLLoss()
 
-    def evaluate(self, outputs, labels):
-        y_pred=[]
-        for a in outputs:
-            y_pred.append(np.argmax(a))
-        y_true = np.array(labels).flatten()
-        y_pred = np.array(y_pred)
-        mat = confusion_matrix(y_true, y_pred)
-        acc = accuracy_score(y_true, y_pred)
-        mcc = matthews_corrcoef(y_true, y_pred)
+    def predict(self, test_loader):
 
-        print('Test: ')
-        print(mat)
-        print('Accuracy = %.3f ,MCC = %.3f' % (acc, mcc))
-
-        return mat, acc, mcc
-
+        scores = []
+        self.eval()
+        for n, (batch, batch_flatten) in enumerate(test_loader):
+            actual_batch_size = len(batch)  # last iteration may contain less sequences
+            seq_len = [len(seq) for seq in batch]
+            seq_len_cumsum = np.cumsum(seq_len)
+            out = self.forward(batch)
+            out = np.vstack(out.detach().numpy())
+            out = np.split(out, seq_len_cumsum)[:-1]
+            batch_scores = []
+            for i in range(actual_batch_size):
+                pos_scores = []
+                for j in range(seq_len[i]):
+                    pos_scores.append(out[i][j, batch[i][j]])
+                batch_scores.append(-sum(pos_scores) / seq_len[i])
+            scores.append(batch_scores[0])
+        return scores
 
 if __name__ == '__main__':
 
-    para_dict = {'num_samples': 1000,
-                 'seq_len': 10,
-                 'model_name': 'LSTM_Bi',
+    para_dict = {'model_name': 'LSTM_Bi',
                  'optim_name': 'Adam',
                  'step_size': 10,
-                 'epoch': 20,
-                 'batch_size' : 20,
+                 'epoch': 5,
+                 'batch_size' : 25000,
                  'learning_rate': 0.01,
                  'gapped':True,
                  'embedding_dim':64,
                  'hidden_dim':64,
                  'fixed_len':True}
 
-    train_loader, test_loader = loader.synthetic_data_loader(num_samples=para_dict['num_samples'], seq_len=para_dict['seq_len'], aa_list='ACDEFGHIKLMNPQRSTVWY_', test_size=0.3, batch_size=para_dict['batch_size'])
+    train_data, test_data = OAS_data_loader.OAS_data_loader(
+        index_file='./Models_generation/Benchmarks/OAS_dataset/data/OAS_meta_info.txt', output_field='Species',
+        input_type='CDR3')
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=para_dict['batch_size'], drop_last=True, collate_fn=collate_fn)
     para_dict['in_dim'] = len(aa2id_i[para_dict['gapped']])
     para_dict['out_dim'] =  len(aa2id_o[para_dict['gapped']])
     model = LSTM_Bi(para_dict)
     model.fit(train_loader)
-    # print(test_loader)
-    output = model.predict(test_loader)
-    labels = np.vstack([i for _, i in test_loader])
-
-    # len(labels)
-    mat, acc, mcc = model.evaluate(output, labels)
+    test_loader = torch.utils.data.DataLoader(test_data, collate_fn=collate_fn)
+    output = model.evaluate1(test_loader)
+    f = open('temp.txt','w')
+    for a in output:
+        f.write(str(a)+'\n')
+    f.close()
