2334641ce61f694e75d3720b1fa8415de511d7e0
dingqy14 <dingqy14@dingqy14deMacBook-Pro.local>
2020-02-18 22:30:43 +0800

    add methods and demo codes for optimization

--- Models/Liu2019/CNNx1_classifier.py
+++ Models/Liu2019/CNNx1_classifier.py
@@ -1,6 +1,6 @@
 #from ..Utils.model import Model
 #from ..Benchmarks.Liu2019_enrichment.Liu2019_data_loader import train_test_loader, encode_data
-from model import Model
+from model import Model 
 import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix, matthews_corrcoef, accuracy_score
@@ -9,6 +9,8 @@ from sklearn.metrics import confusion_matrix, matthews_corrcoef, accuracy_score
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+import pdb
 
 class CNN_classifier(Model):
     def __init__(self, para_dict, *args, **kwargs):
@@ -49,6 +51,73 @@ class CNN_classifier(Model):
 
         return out
 
+    def forward4predict(self, Xs):
+        batch_size = len(Xs)
+
+        X = torch.FloatTensor(Xs)
+        X = X.permute(0,2,1)
+        
+        out = self.conv1(X)
+        out = self.pool(out)
+        out = out.reshape(batch_size, -1)
+        out = F.relu(self.fc1(out))
+        out = torch.sigmoid(self.fc2(out))
+        
+        return out
+    
+    def forward4optim(self, Xs):
+        
+        # turns off the gradient descent for all params
+        for param in self.parameters():
+            param.requires_grad = False
+        
+        batch_size = len(Xs)
+
+        X = torch.FloatTensor(Xs)
+        X = X.permute(0,2,1)
+        self.X_variable = torch.autograd.Variable(X, requires_grad = True)
+        
+        out = self.conv1(self.X_variable)
+        out = self.pool(out)
+        out = out.reshape(batch_size, -1)
+        out = F.relu(self.fc1(out))
+        out = torch.sigmoid(self.fc2(out))
+        
+        return out
+
+    def get_gradient(self, Xs, labels):
+        out = self.forward4optim(Xs)
+        
+        loss_func = self.objective()
+        optim_x_iter = [self.X_variable].__iter__()
+        optimizers = optim.RMSprop(optim_x_iter, lr=self.para_dict['learning_rate'])
+        loss = loss_func(out, torch.tensor(labels).type(torch.long))
+        loss.backward()
+
+        return self.X_variable.grad
+    
+    def optimization(self, seed_seqs, labels, step_size, interval, iteration):
+        
+        loss_func = self.objective()
+        for i in range(iteration):
+            Xs = seed_seqs
+            out = self.forward4optim(Xs)
+            optim_x_iter = [self.X_variable].__iter__()
+            
+            # gradient ascent
+            loss = loss_func(out, torch.tensor(labels).type(torch.long))
+            self.X_variable.grad = torch.zeros(self.X_variable.shape)
+            loss.backward()
+            Xs = (self.X_variable + self.X_variable.grad * step_size).permute(0,2,1)
+            
+            if i % interval == 0:
+                # projection to one-hot representation after K interval
+                position_mat = self.X_variable.argmax(dim = 1).unsqueeze(dim=1)
+                Xs = torch.zeros(self.X_variable.shape).scatter_(1, position_mat, 1)
+                Xs = Xs.permute(0,2,1)
+            
+        return Xs
+
     def fit(self, data_loader):
 
         self.net_init()
@@ -80,6 +149,18 @@ class CNN_classifier(Model):
                 labels = np.concatenate([i for _, i in data_loader])
                 _, _, _, = self.evaluate(np.concatenate(outputs_train), labels)
     
+    def fit4optim(self, seed_seq_loader, iteration, step):
+
+        loss_func = self.objective()
+        seed_seq, labels = seed_seq_loader
+        optimizer = optim.RMSprop(self.parameters(), lr=step)
+
+        for e in range(iteration):
+            logps = self.forward4optim(seed_seq)
+            loss = loss_func(logps, torch.tensor(labels).type(torch.long))
+            optimizer.zero_grad()
+            loss.backward()
+
     def evaluate(self, outputs, labels):
         y_pred = []
         # print(outputs.shape)
@@ -99,6 +180,22 @@ class CNN_classifier(Model):
         print(mat)
         print('Accuracy = %.3f ,MCC = %.3f' % (acc, mcc))
         return mat, acc, mcc
+
+    def predict(self, data_loader):
+
+        self.eval()
+        test_loss = 0
+        all_outputs = []
+        labels_test = []
+        with torch.no_grad():
+            for data in data_loader:
+                # print(data)
+                inputs, _ = data
+                outputs = self.forward4predict(inputs)
+                all_outputs.append(outputs.detach().numpy())
+                # labels_test.append(np.array(l))
+
+            return np.vstack(all_outputs)
 #----------------------------------------------------------
 if __name__ == '__main__':
     traindat = pd.read_csv('cdr3s.table.csv')
@@ -152,4 +249,18 @@ if __name__ == '__main__':
     labels = np.concatenate([i for _, i in test_loader])
     mat, acc, mcc = model.evaluate(output, labels)
 
-
+    # demo for optimization
+    para_json = './work/Seq_32x1_16_class-0.0001/train_parameters.json'
+    para_dict = json.load(open(para_json, 'r'))
+    prev_model = CNN_classifier(para_dict)
+    prev_model.net_init()
+    prev_model.load_model()
+    seed_seqs, labels = next(iter(train_loader))
+    new_seqs = prev_model.optimization(seed_seqs, labels, 
+                                       step_size = 0.001, interval = 10, iteration = 1)
+    # check sequence identity
+    new_seqs.permute(0,2,1).argmax(dim = 1).unsqueeze(dim=1)[0,:,:]
+    batch.permute(0,2,1).argmax(dim = 1).unsqueeze(dim=1)[0,:,:]
+    
+    
+    
