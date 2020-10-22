1efc5cec61f8ae077abeb7d5929f8e21d0bc397c
Roger Jin <rogerjin@mit.edu>
2020-10-22 06:02:15 +0000

    pca embeddings

---
+++ AIPT/Models/Beshnova2020/pca_embedding.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import torch
+import AIPT.Utils.Dev.dev_utils as dev_utils
+
+DEBUG_MODE = True
+if DEBUG_MODE:
+    dev_utils.get_mod_time(__file__, verbose=True)
+
+AA_PCA_FEATURES = pd.read_csv('AAidx_PCA.txt', sep='\t').sort_index()
+
+def embed_seq(aa_seq):
+    encodings = []
+    index = AA_PCA_FEATURES.loc if type(aa_seq) == str else AA_PCA_FEATURES.iloc
+    for aa in aa_seq:
+        encodings.append(index[aa.item()])
+    encodings =  np.vstack(encodings)
+    return torch.Tensor(encodings)
+
+def embed_batch(aa_seq_batch):
+    encoded_batch = map(embed_seq, aa_seq_batch)
+    batch =  torch.stack(tuple(encoded_batch))
+    return batch
+
+
+
+
