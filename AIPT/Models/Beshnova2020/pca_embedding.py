5b88030b71f5683688577b5383cccb5dd35688a7
Roger Jin <rogerjin@mit.edu>
2020-10-22 17:03:41 -0700

    functionality for saving plots

--- AIPT/Models/Beshnova2020/pca_embedding.py
+++ AIPT/Models/Beshnova2020/pca_embedding.py
@@ -7,7 +7,7 @@ DEBUG_MODE = True
 if DEBUG_MODE:
     dev_utils.get_mod_time(__file__, verbose=True)
 
-AA_PCA_FEATURES = pd.read_csv('AAidx_PCA.txt', sep='\t').sort_index()
+AA_PCA_FEATURES = pd.read_csv('AAidx_PCA.txt', sep='\t').sort_index().to_numpy()
 
 def embed_seq(aa_seq):
     encodings = []
@@ -17,9 +17,8 @@ def embed_seq(aa_seq):
     encodings =  np.vstack(encodings)
     return torch.Tensor(encodings)
 
-def embed_batch(aa_seq_batch):
-    encoded_batch = map(embed_seq, aa_seq_batch)
-    batch =  torch.stack(tuple(encoded_batch))
-    return batch
-
-def embedding
+def embedding_fn(num_embeddings, embedding_dim):
+    torch_embedding_fn = torch.nn.Embedding(num_embeddings, embedding_dim)
+    torch_embedding_fn.weight.data.copy_(torch.from_numpy(AA_PCA_FEATURES))
+    torch_embedding_fn.requires_grad = False
+    return torch_embedding_fn
