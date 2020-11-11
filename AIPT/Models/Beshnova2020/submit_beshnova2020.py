7bd2b0bfc45fa0938bc58175de64945fef5750bb
Roger Jin <rogerjin@mit.edu>
2020-11-11 04:22:10 -0800

    move submit script to model dir

---
+++ AIPT/Models/Beshnova2020/submit_beshnova2020.py
@@ -0,0 +1,8 @@
+'''
+To submit a training or tuning job to SageMaker, copy this file into the root "antibody-in-pytorch" directory,
+and then run the desired cells in "Beshnova2020/sagemaker.ipynb".
+'''
+
+from AIPT.Models.Beshnova2020.train_job import main
+
+main()
--- submit_beshnova2020.py
+++
@@ -1,3 +0,0 @@
-from AIPT.Models.Beshnova2020.train_job import main
-
-main()
